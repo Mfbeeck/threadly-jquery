2afc84dd52c90f48844d5a3b5b4bbdb4193b820f
Mfbeeck <mfbeeck@gmail.com>
2015-07-09 19:31:44 -0400

    threadly exercise

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var template = function(comment){
+	return  '<li>' + comment + '</li>'
+}
+
 var main = function() {
-  $('form').submit(function() {
-    
-    if(comment !== "") {
-      var html = $('<li>').text(comment);
-      
+  $('form').on('submit',function() {
+    var comment = $('#comment').val();
+    if(comment !== "") {	
+      // var html = $('<li>').text(comment);
+      var html = template(comment);
+      $('.comments').prepend(html);
+      // prepend appends to the top of a section
+      $('#comment').val('');
     }
+    else $('#comment').val('');
 
     return false;
   });
